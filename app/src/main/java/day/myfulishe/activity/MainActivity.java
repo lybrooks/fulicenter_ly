a1eeed75daa5cc1b4b4a3bd71a665f387379b8c2
lybrooks <506119809@qq.com>
2016-10-14 20:13:14 +0800

    解决RadiogRroup的BUG，添加fragment

--- app/src/main/java/day/myfulishe/activity/MainActivity.java
+++ app/src/main/java/day/myfulishe/activity/MainActivity.java
@@ -1,14 +1,24 @@
 package day.myfulishe.activity;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.RadioButton;
 
 import day.myfulishe.R;
+import myFragment.fragment_boutique;
+import myFragment.fragment_cart;
+import myFragment.fragment_category;
+import myFragment.fragment_newgoods;
+import myFragment.fragment_personal;
 
 public class MainActivity extends AppCompatActivity {
     RadioButton mrb_item_newgood, mrb_item_boutique, mrb_item_category, mrb_item_cart, mrb_item_personal;
+    FragmentManager fragmentManager;
+    FragmentTransaction transaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,24 +33,45 @@ public class MainActivity extends AppCompatActivity {
         mrb_item_category = (RadioButton) findViewById(R.id.rb_item_category);
         mrb_item_cart = (RadioButton) findViewById(R.id.rb_item_cart);
         mrb_item_personal = (RadioButton) findViewById(R.id.rb_item_personal);
+        fragmentManager = getSupportFragmentManager();
     }
 
     public void onCheckedChange(View v) {
         switch (v.getId()) {
             case R.id.rb_item_newgood:
                 Change((RadioButton) v);
+                fragment_newgoods newgoods = new fragment_newgoods();
+                transaction = fragmentManager.beginTransaction();
+                transaction.replace(R.id.Linear, newgoods);
+                transaction.commit();
                 break;
             case R.id.rb_item_boutique:
                 Change((RadioButton) v);
+                fragment_boutique boutique = new fragment_boutique();
+                transaction = fragmentManager.beginTransaction();
+                transaction.replace(R.id.Linear, boutique);
+                transaction.commit();
                 break;
             case R.id.rb_item_category:
                 Change((RadioButton) v);
+                fragment_category category = new fragment_category();
+                transaction = fragmentManager.beginTransaction();
+                transaction.replace(R.id.Linear, category);
+                transaction.commit();
                 break;
             case R.id.rb_item_cart:
                 Change((RadioButton) v);
+                fragment_cart cart = new fragment_cart();
+                transaction = fragmentManager.beginTransaction();
+                transaction.replace(R.id.Linear, cart);
+                transaction.commit();
                 break;
             case R.id.rb_item_personal:
                 Change((RadioButton) v);
+                fragment_personal personal = new fragment_personal();
+                transaction = fragmentManager.beginTransaction();
+                transaction.replace(R.id.Linear, personal);
+                transaction.commit();
                 break;
 
 
