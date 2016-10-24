1eb57e58561b1c946f54d8f9d86d63ea220a7c0a
lybrooks <506119809@qq.com>
2016-10-24 19:56:16 +0800

    登录成功跳转个人中心

--- app/src/main/java/day/myfulishe/activity/FuLiCenterApplication.java
+++ app/src/main/java/day/myfulishe/activity/FuLiCenterApplication.java
@@ -2,12 +2,34 @@ package day.myfulishe.activity;
 
 import android.app.Application;
 
+import cn.ucai.fulicenter.bean.UserBean;
+
 /**
  * Created by Administrator on 2016/10/17.
  */
 public class FuLiCenterApplication extends Application {
     private static FuLiCenterApplication instance;
     public static FuLiCenterApplication application;
+
+    public String getUsernane() {
+        return Usernane;
+    }
+
+    public void setUsernane(String usernane) {
+        Usernane = usernane;
+    }
+
+    String Usernane;
+
+    public static UserBean getUserBean() {
+        return userBean;
+    }
+
+    public  void setUserBean(UserBean userBean) {
+     this.userBean=userBean;
+    }
+
+    public static UserBean userBean;
 
     @Override
     public void onCreate() {
