5351571658e2a647f97a3b57b04c9b07fbe453af
lybrooks <506119809@qq.com>
2016-10-25 10:18:47 +0800

    添加个人资料布局，以及个人信息显示

--- app/src/main/java/day/myfulishe/activity/MySetting.java
+++ app/src/main/java/day/myfulishe/activity/MySetting.java
@@ -1,15 +1,47 @@
 package day.myfulishe.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import cn.ucai.fulicenter.utils.ImageLoader;
 import day.myfulishe.R;
 
 public class MySetting extends AppCompatActivity {
+
+    @Bind(R.id.iv_User_avatar)
+    ImageView ivUserAvatar;
+    @Bind(R.id.tv_Username)
+    TextView tvUsername;
+    @Bind(R.id.iv_update_name)
+    ImageView ivUpdateName;
+    @Bind(R.id.tv_UserNick)
+    TextView tvUserNick;
+    @Bind(R.id.iv_update_nick)
+    ImageView ivUpdateNick;
+    @Bind(R.id.bt_Quit)
+    Button btQuit;
+
+    MySetting mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_setting);
+        ButterKnife.bind(this);
+        mContext = this;
+        initView();
+    }
+
+    private void initView() {
+        String muserName = FuLiCenterApplication.getUserBean().getMuserName();
+        tvUsername.setText(muserName);
+        String muserNick = FuLiCenterApplication.getUserBean().getMuserNick();
+        tvUserNick.setText(muserNick);
+        ImageLoader.setAcatar(ImageLoader.getAcatarUrl(FuLiCenterApplication.getUserBean()), mContext, ivUserAvatar);
     }
 }
